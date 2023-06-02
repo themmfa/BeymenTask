172d4c5349b0b7d0df2254e0daa7620cd46cc459
fe <fatiherdgn25@gmail.com>
2023-06-02 13:37:50 +0300

    Initial Commit

---
+++ BeymenTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeymenTask
+//
+//  Created by fe on 2.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
